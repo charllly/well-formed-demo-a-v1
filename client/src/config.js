4679cc96d0c480644bf15ca80f9868fcd898727e
charllly <charllly.dev@gmail.com>
2023-06-06 15:56:46 -0700

    first commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://well-formed-api.onrender.com'
+export const baseUrl = 'https://well-formed-api.onrender.com'
 export const randomCoverImage = 'https://source.unsplash.com/random?wallpapers'
 export const demoEventSlug = 'jamstack4good22'
