d0901ea0f9a9e69093a2cf10caeaf704da760850
charllly <charllly.dev@gmail.com>
2023-06-06 15:20:51 -0700

    first commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://localhost:5000'
+export const baseUrl = 'http://well-formed-api.onrender.com'
 export const randomCoverImage = 'https://source.unsplash.com/random?wallpapers'
 export const demoEventSlug = 'jamstack4good22'
